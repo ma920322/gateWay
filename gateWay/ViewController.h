bbeeaa1344a043202642440dfc091cd407b1a769
ma920322 <2291190232@qq.com>
2016-11-22 23:20:05 +0800

    Initial Commit

---
+++ gateWay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gateWay
+//
+//  Created by MaXing on 16/11/22.
+//  Copyright © 2016年 MaXing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gateWay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gateWay
+//
+//  Created by MaXing on 16/11/22.
+//  Copyright © 2016年 MaXing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
